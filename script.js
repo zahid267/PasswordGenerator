e9c040362c3dbf7a7865ba23f289677c98133b34
Muhammad Zahid <mrzahid@rogers.com>
2021-03-13 19:20:47 -0500

    Updating css and javascript files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
+  passwordText.value = "";
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -23,56 +23,72 @@ function generatePassword(){
   var reqresult = "";   /// All Required characters
   var randomChars = "";
   var passlen = promptLen();
+  if(passlen < 8 || passlen>128){
+      return "";
+  }
   
   var numok = promptCnum("numeric");
   if(numok){  // Select 1 numeric character
     result = getRandomString(1,result,numChars)
     randomChars += numChars;
   }
+  
   var lcok = promptCnum("lowercase");
   if(lcok){   // Select 1 lower character
     result = getRandomString(1,result,lcChars)
     randomChars += lcChars;
   }
+  
   var ucok = promptCnum("uppercase");
   if(ucok){   // Select 1 uppercase character
     result = getRandomString(1,result,ucChars)
     randomChars += ucChars;
   }
+  
   var scok = promptCnum("special");
   if(scok){   // Select 1 special character
     result = getRandomString(1,result,scChars)
     randomChars += scChars;
   }
+  
   if(randomChars === ""){
     alert("You should select at least one character category to generate password. No password is generated.\nTry again.");
     return "";
   }
+  
+  //console.log(passlen + " -----result-len " +  result.length)
 
   passlen = passlen - result.length;   /// find the password length for remaining characters (total characters - already selected)
+  
   reqresult = result;
   result = "";
   /// now select remaining characters from the mixed categories of characters
+  
   result = getRandomString(passlen,result,randomChars)
-
+  
   /// now ramdomly insert the required characters in the result sctring at a random position
   var reqpos = Math.floor(Math.random() * result.length);
   result = result.slice(0, reqpos) + reqresult + result.slice(reqpos);
 
-  //alert(reqresult+"\n"+result + "\n"+result.length);
+  //console.log(reqresult+"\nselect-all : "+result + "\nrel-len : "+result.length);
 
   return result;
 
 
 }
 function promptLen(){
-  var passlen = prompt("How many characters would you like your password to contain?\nEnter a number between 8 and 128.");
-    if(passlen >= 8 && passlen<=128){
-      return passlen;
+  var passlen2 = prompt("How many characters would you like your password to contain?\nEnter a number between 8 and 128.");
+    if(passlen2 >= 8 && passlen2 <= 128){
+      //return passlen2;
     }else{
-      alert("The Password length should be from 8 to 128. \nRe-enter the password length.");
-      promptLen();
+      var cont = confirm("The Password length should be from 8 to 128. \nRe-enter the password length.\nClick OK to Continue?");
+      if(cont){
+        passlen2 = promptLen();
+      }else{
+        passlen2 = 0;
+      }
     }
+    return passlen2;
 }
 function promptCnum(nameType){
   var sok = confirm("Click OK to confirm including "+nameType+" characters.");
